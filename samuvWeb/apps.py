8af2155c2a8e34dfa36e0586281e528a1f8f19d9
MaxxReis <marcelo.dalta@gmail.com>
2018-08-07 15:17:02 -0300

    django test, first commit

---
+++ samuvWeb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SamuvwebConfig(AppConfig):
+    name = 'samuvWeb'
---
+++ vmSamuv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/qt/testeSamuv/vmSamuv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
